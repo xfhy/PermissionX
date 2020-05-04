12f3b287319327963594352ccadca55eec066256
xfhy <xfhy666@qq.com>
2020-05-04 22:43:15 +0800

    test library

--- app/src/main/java/com/xfhy/permissionx/MainActivity.kt
+++ app/src/main/java/com/xfhy/permissionx/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.xfhy.permissionx
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.permissionx.xfhydev.PermissionX
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun call(view: View) {
+        PermissionX.request(this, Manifest.permission.CALL_PHONE) { allGranted, deniedList ->
+            if (allGranted) {
+                callPhone()
+            } else {
+                Toast.makeText(this, "请授权", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun callPhone() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
     }
 }
